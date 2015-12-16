b94fd73d801f7ca70596cee8b3af1a4961d07b1f
Roel958 <roelvanwely958@gmail.com>
2015-12-16 11:12:02 +0100

    bla

--- Jonkman_vanWelyConc3/src/Koper.java
+++ Jonkman_vanWelyConc3/src/Koper.java
@@ -1,4 +1,29 @@
 
 public class Koper extends Thread{
+	private int koperId;
+	private Rai rai;
+	
+	public Koper() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void run() {
+		try {
+			while(true){
+			//dingen
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void aanmelden(){
+		rai.getFloor().joinAsVisitor(this);
+	}
+	
+	public int getKijkerId(){
+		return this.koperId;
+	}
 
 }
